be1bc8cc5e93b7c44840d005eabb74fed0163e37
hridoy-ahmmad <hridoyp338y@gmail.com>
2026-06-29 15:21:38 +0600

    Add registration page with form validation
    - Add full name, email, password, confirm password, and role fields
    - Add react-hook-form validation
    - Add password strength rules
    - Add confirm password matching validation
    - Add show/hide password checkbox
    - Add dark themed registration UI
    - Add Google continue button and login link

---
+++ src/Components/Nav.jsx
@@ -0,0 +1,162 @@
+
+'use client';
+
+import { useState } from "react";
+import { Link, Button } from "@heroui/react";
+import Image from "next/image";
+
+export default function Nav() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const navItems = (
+    <>
+      <li>
+        <Link
+          href="/"
+          className="text-sm font-medium text-zinc-300 transition-all duration-300 hover:text-white"
+        >
+          Home
+        </Link>
+      </li>
+
+      <li>
+        <Link
+          href="#"
+          className="text-sm font-medium text-zinc-300 transition-all duration-300 hover:text-white"
+        >
+          Dashboard
+        </Link>
+      </li>
+      <li>
+        <Link
+          href="#"
+          className="text-sm font-medium text-zinc-300 transition-all duration-300 hover:text-white"
+        >
+          Pricing
+        </Link>
+      </li>
+    </>
+  );
+
+  return (
+    <nav className="fixed top-0 left-0 z-50 w-full border-b border-white/10 bg-black/40 backdrop-blur-2xl">
+      <header className="mx-auto flex h-20 max-w-7xl items-center justify-between px-5 lg:px-8">
+        {/* Logo */}
+        <Link href="/" className="flex items-center gap-2">
+          <Image
+            src="/logo.png"
+            alt="Eventora Logo"
+            width={42}
+            height={42}
+            priority
+          />
+
+          <span className="text-2xl font-bold tracking-wide text-white">
+            Eventora
+          </span>
+        </Link>
+
+        {/* Desktop Menu */}
+        <ul className="hidden items-center gap-10 md:flex">
+          {navItems}
+        </ul>
+
+        {/* Right Side */}
+        <div className="flex items-center gap-3">
+
+          {/* Desktop Buttons */}
+          <div className="hidden items-center gap-4 md:flex">
+            <Link
+              href="#"
+              className="text-sm font-medium text-zinc-300 transition hover:text-white"
+            >
+              Login
+            </Link>
+
+            <Button
+              className="w-full rounded-lg bg-linear-to-r from-[#3B1DFF] via-[#6A1BFF] to-[#FF3DA8] py-3 font-semibold text-white transition-all duration-300 hover:shadow-lg  hover:scale-[1.02]"
+            >
+              <Link className={'hover:no-underline'}>
+                Sign Up
+              </Link>
+
+            </Button>
+          </div>
+
+          {/* Mobile Menu Button */}
+          <button
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+            className="flex h-11 w-11 items-center justify-center rounded-xl border border-white/10 bg-white/5 text-white transition-all duration-300 hover:bg-white/10 md:hidden"
+            aria-label="Toggle Menu"
+          >
+            {isMenuOpen ? (
+              <svg
+                className="h-6 w-6"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            ) : (
+              <svg
+                className="h-6 w-6"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+            )}
+          </button>
+
+        </div>
+      </header>
+
+      {/* Mobile Menu */}
+      <div
+        className={`overflow-hidden transition-all duration-300 ease-in-out md:hidden ${isMenuOpen ? "max-h-96" : "max-h-0"
+          }`}
+      >
+        <div className="border-t border-white/10 bg-zinc-950/95 backdrop-blur-2xl">
+          <ul className="flex flex-col gap-6 px-6 py-6">
+
+            {navItems}
+
+            <li>
+              <Link
+                href="#"
+                className="text-sm font-medium text-zinc-300 transition hover:text-white"
+              >
+                Login
+              </Link>
+            </li>
+
+            <li>
+                 <Button
+              className="w-full rounded-lg bg-linear-to-r from-[#3B1DFF] via-[#6A1BFF] to-[#FF3DA8] py-3 font-semibold text-white transition-all duration-300 hover:shadow-lg  hover:scale-[1.02]"
+            >
+              <Link className={'hover:no-underline'}>
+                Sign Up
+              </Link>
+
+            </Button>
+            </li>
+
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
---
+++ src/app/RegisterForm/page.jsx
@@ -0,0 +1,209 @@
+"use client";
+
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+export default function RegisterForm() {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+    reset
+  } = useForm();
+  const password = watch("password");
+  const onSubmit = (data) => {
+    console.log(data);
+    reset()
+  };
+
+  return (
+    <div className="min-h-screen bg-black flex items-center justify-center px-4 py-16">
+      <div className="w-full max-w-md rounded-2xl border border-gray-800 bg-[#111111] p-8 shadow-2xl">
+        <h2 className="mb-2 text-center text-3xl font-bold text-white">
+          Create Account
+        </h2>
+
+        <p className="mb-8 text-center text-gray-400">
+          Join Eventora and start managing events effortlessly.
+        </p>
+
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="space-y-5 text-gray-300"
+        >
+          {/* Name */}
+          <div>
+            <label className="mb-2 block text-sm font-medium">
+              Full Name
+            </label>
+
+            <input
+              {...register("fullName", {
+                required: "Full name is required",
+              })}
+              type="text"
+              placeholder="John Doe"
+              className="w-full rounded-lg border border-gray-700 bg-[#1a1a1a] px-4 py-3 text-white placeholder-gray-500 outline-none transition focus:border-blue-500 focus:ring-2 focus:ring-blue-500/30"
+            />
+
+            {errors.fullName && (
+              <p className="mt-1 text-sm text-red-500">
+                {errors.fullName.message}
+              </p>
+            )}
+          </div>
+
+          {/* Email */}
+          <div>
+            <label className="mb-2 block text-sm font-medium">
+              Email Address
+            </label>
+
+            <input
+              {...register("email", {
+                required: "Email is required",
+              })}
+              type="email"
+              placeholder="john@example.com"
+              className="w-full rounded-lg border border-gray-700 bg-[#1a1a1a] px-4 py-3 text-white placeholder-gray-500 outline-none transition focus:border-blue-500 focus:ring-2 focus:ring-blue-500/30"
+            />
+
+            {errors.email && (
+              <p className="mt-1 text-sm text-red-500">
+                {errors.email.message}
+              </p>
+            )}
+          </div>
+
+          {/* Password */}
+          <div>
+            <label className="mb-2 block text-sm font-medium">
+              Password
+            </label>
+
+            <input
+              {...register("password", {
+                required: "Password is required",
+                minLength: {
+                  value: 8,
+                  message: "Password must be at least 8 characters",
+                },
+                pattern: {
+                  value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).+$/,
+                  message:
+                    "Password must contain uppercase, lowercase, number and special character",
+                },
+              })}
+              type={showPassword ? "text" : "password"}
+              placeholder="Enter your password"
+              className="w-full rounded-lg border border-gray-700 bg-[#1a1a1a] px-4 py-3 text-white placeholder-gray-500 outline-none transition focus:border-blue-500 focus:ring-2 focus:ring-blue-500/30"
+            />
+
+            {errors.password && (
+              <p className="mt-1 text-sm text-red-500">
+                {errors.password.message}
+              </p>
+            )}
+          </div>
+
+          {/* Confirm Password */}
+          <div>
+            <label className="mb-2 block text-sm font-medium">
+              Confirm Password
+            </label>
+
+            <input
+              {...register("confirmPassword", {
+                required: "Confirm Password is required",
+                validate: (value) =>
+                  value === password || "Passwords do not match",
+              })}
+              type={showPassword ? "text" : "password"}
+              placeholder="Confirm your password"
+              className="w-full rounded-lg border border-gray-700 bg-[#1a1a1a] px-4 py-3 text-white placeholder-gray-500 outline-none transition focus:border-blue-500 focus:ring-2 focus:ring-blue-500/30"
+            />
+
+            {errors.confirmPassword && (
+              <p className="mt-1 text-sm text-red-500">
+                {errors.confirmPassword.message}
+              </p>
+            )}
+          </div>
+
+          {/* Role */}
+          <div>
+            <label className="mb-2 block text-sm font-medium">
+              Select Your Role
+            </label>
+
+            <select
+              {...register("role", {
+                required: "Please select a role",
+              })}
+              className="w-full rounded-lg border border-gray-700 bg-[#1a1a1a] px-4 py-3 text-white outline-none transition focus:border-blue-500 focus:ring-2 focus:ring-blue-500/30"
+            >
+              <option value="">Choose a role</option>
+              <option value="attendee">Attendee</option>
+              <option value="organizer">Organizer</option>
+            </select>
+
+            {errors.role && (
+              <p className="mt-1 text-sm text-red-500">
+                {errors.role.message}
+              </p>
+            )}
+          </div>
+
+          {/* Show Password */}
+          <div className="flex items-center gap-2">
+            <input
+              id="showPassword"
+              type="checkbox"
+              checked={showPassword}
+              onChange={() => setShowPassword(!showPassword)}
+              className="h-4 w-4 accent-blue-600"
+            />
+
+            <label
+              htmlFor="showPassword"
+              className="text-sm text-gray-400 cursor-pointer"
+            >
+              Show Password
+            </label>
+          </div>
+
+          {/* Submit */}
+          <button
+            type="submit"
+            className="w-full rounded-lg bg-linear-to-r from-[#3B1DFF] via-[#6A1BFF] to-[#FF3DA8] py-3 font-semibold text-white transition-all duration-300 hover:shadow-lg hover:shadow-purple-500/40 hover:scale-[1.02] "
+          >
+            Create Account
+          </button>
+        </form>
+
+        <div className="my-6 flex items-center">
+          <div className="h-px flex-1 bg-gray-700"></div>
+          <span className="mx-4 text-sm text-gray-500">OR</span>
+          <div className="h-px flex-1 bg-gray-700"></div>
+        </div>
+
+        <button className="w-full rounded-lg border border-gray-700 bg-[#1a1a1a] py-3 font-medium text-white transition hover:bg-[#242424]">
+          Continue with Google
+        </button>
+
+        <p className="mt-6 text-center text-sm text-gray-400">
+          Already have an account?{" "}
+          <button
+            type="button"
+            className="font-semibold text-blue-500 transition hover:text-blue-400"
+          >
+            Login
+          </button>
+        </p>
+      </div>
+    </div>
+  );
+}
